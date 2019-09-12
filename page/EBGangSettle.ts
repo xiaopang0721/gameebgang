b856719b596d6a3ecfdf7d21031c09bcb8b6a2b3
xiaoer <xiaoer@example.com>
2019-09-12 20:14:32 +0800

    结算界面色值修改

--- page/EBGangSettle.ts
+++ page/EBGangSettle.ts
@@ -112,12 +112,12 @@ module gameebgang.page {
             this.lab_money.text = this._data.money;
             // this.lab_bankermultiple.text = this._data.isbanker ? this._data.bankermultiple : "0";
             // this.lab_cardtype.text = this._data.cardtype;
-            this.lab_name.color = this._data.isMain ? "#cc90ff" : "#ffffff";
-            this.lab_point.color = this._data.isMain ? "#cc90ff" : "#ffffff";
-            this.lab_multiple.color = this._data.isMain ? "#cc90ff" : "#ffffff";
-            this.lab_money.color = parseFloat(this._data.money) >= 0 ? "#069e00" : "#ff0000";
-            // this.lab_bankermultiple.color = this._data.isMain ? "#cc90ff" : "#ffffff";
-            // this.lab_cardtype.color = this._data.isMain ? "#cc90ff" : "#ffffff";
+            this.lab_name.color = this._data.isMain ? TeaStyle.COLOR_JIESUAN : "#ffffff";
+            this.lab_point.color = this._data.isMain ? TeaStyle.COLOR_JIESUAN : "#ffffff";
+            this.lab_multiple.color = this._data.isMain ? TeaStyle.COLOR_JIESUAN : "#ffffff";
+            this.lab_money.color = parseFloat(this._data.money) >= 0 ? TeaStyle.COLOR_JIESUAN : TeaStyle.COLOR_RED;
+            // this.lab_bankermultiple.color = this._data.isMain ? TeaStyle.COLOR_JIESUAN : "#ffffff";
+            // this.lab_cardtype.color = this._data.isMain ? TeaStyle.COLOR_JIESUAN : "#ffffff";
         }
 
         destroy() {
