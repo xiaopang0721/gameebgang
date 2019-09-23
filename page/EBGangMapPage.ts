f1808676846c077b4414c66f61f5357d50a32d3b
happy <happy>
2019-09-23 20:03:47 +0800

    二八杠

--- page/EBGangMapPage.ts
+++ page/EBGangMapPage.ts
@@ -102,6 +102,7 @@ module gameebgang.page {
                 this._EBGangMgr.on(EBGangMgr.CONTINUE_GAME, this, this.onClickContinueGame);
             }
             this._game.playMusic(Path_game_ebgang.music_ebgang + MUSIC_PATH.musicBGM);
+            this._viewUI.btn_cards.left = this._game.isFullScreen ? 110 : 90;
             this._viewUI.btn_menu.left = this._game.isFullScreen ? 30 : 10;
             this._viewUI.img_menu.left = this._game.isFullScreen ? 25 : 10;
         }
