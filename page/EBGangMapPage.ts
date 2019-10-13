ce86ebf9a13f95e0a4aba08e10d01242b109bc61
happy <happy>
2019-10-13 20:08:26 +0800

    client批量提交

--- page/EBGangMapPage.ts
+++ page/EBGangMapPage.ts
@@ -401,7 +401,9 @@ module gameebgang.page {
                     viewHead.txt_money.text = EnumToString.getPointBackNum(unit.GetMoney(), 2);
                     // this.GetDoubleFloat(unit.GetMoney());
                     //头像框
-                    viewHead.img_txk.skin = TongyongUtil.getTouXiangKuangUrl(unit.GetHeadKuangImg(), 2);
+                    viewHead.img_txk.skin = TongyongUtil.getTouXiangKuangUrl(unit.GetHeadKuangImg());
+                    viewHead.img_vip.visible = mainUnit.GetVipLevel() > 0;
+                    viewHead.img_vip.skin = TongyongUtil.getVipUrl(unit.GetVipLevel());
                     //祈福成功 头像上就有动画
                     if (qifu_index && posIdx == qifu_index) {
                         viewHead.qifu_type.visible = true;
@@ -489,14 +491,17 @@ module gameebgang.page {
                     this._viewUI.view_head0.img_icon.skin = TongyongUtil.getHeadUrl(mPlayer.playerInfo.headimg, 2);
                     this._viewUI.view_head0.img_qifu.visible = TongyongUtil.getIsHaveQiFu(mPlayer, this._game.sync.serverTimeBys);
                     //头像框
-                    this._viewUI.view_head0.img_txk.skin = TongyongUtil.getTouXiangKuangUrl(mPlayer.playerInfo.headKuang, 2);
+                    this._viewUI.view_head0.img_txk.skin = TongyongUtil.getTouXiangKuangUrl(mPlayer.playerInfo.headKuang);
+                    //vip标识
+                    this._viewUI.view_head0.img_vip.visible = mPlayer.playerInfo.vip_level > 0;
+                    this._viewUI.view_head0.img_vip.skin = TongyongUtil.getVipUrl(mPlayer.playerInfo.vip_level);
                 } else {
                     money = unitOffline.GetMoney();
                     this._viewUI.view_head0.txt_name.text = getMainPlayerName(unitOffline.GetName());
                     this._viewUI.view_head0.img_icon.skin = TongyongUtil.getHeadUrl(unitOffline.GetHeadImg(), 2);
                     this._viewUI.view_head0.img_qifu.visible = TongyongUtil.getIsHaveQiFu(unitOffline, this._game.sync.serverTimeBys);
                     //头像框
-                    this._viewUI.view_head0.img_txk.skin = TongyongUtil.getTouXiangKuangUrl(unitOffline.GetHeadKuangImg(), 2);
+                    this._viewUI.view_head0.img_txk.skin = TongyongUtil.getTouXiangKuangUrl(unitOffline.GetHeadKuangImg());
                 }
                 money = EnumToString.getPointBackNum(money, 2);
                 this._viewUI.view_head0.txt_money.text = money.toString();
